31aefbc47ec45dffa3d3cadd12d613c333937d02
nlkassad <nlkassad@hotmail.com>
2016-09-19 09:25:57 -0400

    Update Test Version of Calculator

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,34 @@
 require 'pry'
 
 # Calculator Class here
+class Calculator
+  def initialize
+    @num_one = number
+  end
+end
+
+# Addition
+def Addition (number)
+  #n + num2
+  puts self.to_i + number
+end
+
+# Subtraction
+def Subtraction (number)
+  #n - num2
+  puts self.to_i - number
+end
+# Multiplication
+def Multiplication (number)
+  #n * num2
+  puts self.to_i * number
+end
+# Division
+def Division (number)
+  #n / num2
+  puts self.to_i / number
+end
+
 
 binding.pry
 ''
